cbf70b6814d137a9531a85a207f45518561304ac
SolbiZZang <thfql5481@naver.com>
2017-05-01 23:27:09 +0900

    회원가입 레이아웃 추가함.

--- app/src/main/java/skhu/cse/network/omni_stadium/MainActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/MainActivity.java
@@ -5,11 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView SignUp = (TextView)findViewById(R.id.tvregister);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
+
 }
