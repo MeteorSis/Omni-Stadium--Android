7240d788d64473578fd724746856833b49be9b39
SolbiZZang <thfql5481@naver.com>
2017-05-02 17:49:20 +0900

    기능별 액티비티와 xml파일 생성했습니다.

---
+++ app/src/main/java/skhu/cse/network/omni_stadium/OrderActivity.java
@@ -0,0 +1,13 @@
+package skhu.cse.network.omni_stadium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_main);
+    }
+}
