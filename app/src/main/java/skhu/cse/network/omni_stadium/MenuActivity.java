bd269e95401757bbaa9f220b1caa21bba7a97332
SolbiZZang <thfql5481@naver.com>
2017-05-02 18:09:38 +0900

    NFC  Activity Changed.

--- app/src/main/java/skhu/cse/network/omni_stadium/MenuActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/MenuActivity.java
@@ -1,5 +1,6 @@
 package skhu.cse.network.omni_stadium;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -68,22 +69,18 @@ public class MenuActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-       /* if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_nfc) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_streaming) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_ticket) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_order) {
 
-        } else if (id == R.id.nav_send) {
-
-        }  */
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
