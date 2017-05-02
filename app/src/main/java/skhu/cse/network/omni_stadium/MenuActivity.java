87ef02c58f8a95a7760816d1ead120d7111f0df7
SolbiZZang <thfql5481@naver.com>
2017-05-02 18:36:57 +0900

    menu activity , string changed

--- app/src/main/java/skhu/cse/network/omni_stadium/MenuActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/MenuActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -71,6 +72,10 @@ public class MenuActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
 
         int id = item.getItemId();
+
+        Fragment frg = null;
+        String title = getString(R.string.app_name);
+
 
         if (id == R.id.nav_nfc) {
 
