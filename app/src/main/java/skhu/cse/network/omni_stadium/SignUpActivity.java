6d4121c97831170d883f9b8b9d736b72e6e391f7
chicken <earlydrink@gmail.com>
2017-05-04 14:17:22 +0900

    signup change

--- app/src/main/java/skhu/cse/network/omni_stadium/SignUpActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/SignUpActivity.java
@@ -4,12 +4,24 @@ package skhu.cse.network.omni_stadium;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        TextView tvLogin = (TextView)findViewById(R.id.link_login);
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
     }
 }
