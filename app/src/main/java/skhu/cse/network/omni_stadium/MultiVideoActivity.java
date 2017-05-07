07218cde89fb85e1bf8951d6daa0c232630f8464
MeteorSis <uncleone@naver.com>
2017-05-07 17:02:29 +0900

    add Independent buffering

--- app/src/main/java/skhu/cse/network/omni_stadium/MultiVideoActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/MultiVideoActivity.java
@@ -16,13 +16,11 @@ public class MultiVideoActivity extends AppCompatActivity {
 
     VideoView videoview1st;
     VideoView videoview2nd;
-    Boolean video1stLoaded = false;
-    Boolean video2ndLoaded = false;
     ProgressBar spinnerView1;
     ProgressBar spinnerView2;
 
     String VideoURL1st = "rtsp://192.168.63.109:8554/test";
-    String VideoURL2nd = "rtsp://192.168.63.109:8554/test";
+    String VideoURL2nd = "rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,35 +56,21 @@ public class MultiVideoActivity extends AppCompatActivity {
         videoview1st.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             // Close the progress bar and play the video
             public void onPrepared(MediaPlayer mp) {
-                video1stLoaded=true;
                 Log.v("Loaded", " video1");
-                if(video2ndLoaded)
-                {
-                    spinnerView1.setVisibility(View.GONE);
-                    spinnerView2.setVisibility(View.GONE);
-                    videoview1st.setAlpha(1.0f);
-                    videoview2nd.setAlpha(1.0f);
-                    videoview1st.start();
-                    videoview2nd.start();
-                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                }
+                spinnerView1.setVisibility(View.GONE);
+                videoview1st.setAlpha(1.0f);
+                videoview1st.start();
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             }
         });
         videoview2nd.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             // Close the progress bar and play the video
             public void onPrepared(MediaPlayer mp) {
-                video2ndLoaded=true;
                 Log.v("Loaded", " video2");
-                if(video1stLoaded)
-                {
-                    spinnerView1.setVisibility(View.GONE);
-                    spinnerView2.setVisibility(View.GONE);
-                    videoview1st.setAlpha(1.0f);
-                    videoview2nd.setAlpha(1.0f);
-                    videoview1st.start();
-                    videoview2nd.start();
-                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                }
+                spinnerView2.setVisibility(View.GONE);
+                videoview2nd.setAlpha(1.0f);
+                videoview2nd.start();
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             }
         });
         videoview1st.setOnTouchListener(new View.OnTouchListener() {
