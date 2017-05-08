4ef7f460f75e8950237888eab6e83be761928116
SolbiZZang <thfql5481@naver.com>
2017-05-08 20:01:21 +0900

    order_main and OrderActivity was Changend

--- app/src/main/java/skhu/cse/network/omni_stadium/OrderActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/OrderActivity.java
@@ -1,5 +1,6 @@
 package skhu.cse.network.omni_stadium;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -7,12 +8,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class OrderActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_main);
+        ImageView Chicken = (ImageView)findViewById(R.id.ivChicken);
+        ImageView Pizza = (ImageView)findViewById(R.id.ivPizza);
+        ImageView Beer = (ImageView)findViewById(R.id.ivBeer);
+        ImageView Hamburger = (ImageView)findViewById(R.id.ivHamburger);
+
+        Chicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Cintent = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(Cintent);
+            }
+        });
+
+       Pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Pintent = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(Pintent);
+            }
+        });
+
+       Beer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Bintent = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(Bintent);
+            }
+        });
+
+       Hamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Hintent = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(Hintent);
+            }
+        });
+
+
+
+
+
     }
 
 }
