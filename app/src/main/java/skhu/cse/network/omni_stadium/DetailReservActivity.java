a95180354a5f79764661c7e5b582504fd0de5fcf
SolbiZZang <thfql5481@naver.com>
2017-05-09 15:27:12 +0900

    회원가입에서 아이디 최대  입력길이  12로 제한.

--- app/src/main/java/skhu/cse/network/omni_stadium/DetailReservActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/DetailReservActivity.java
@@ -1,24 +1,45 @@
 package skhu.cse.network.omni_stadium;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
 import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class DetailReservActivity extends AppCompatActivity {
     Button SeatOk;
+    ToggleButton G1;
+    ToggleButton G2;
+    ToggleButton G3;
+    ToggleButton G4;
+    ToggleButton G5;
+    ToggleButton G6;
+    ToggleButton G7;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reserv_detail);
         SeatOk = (Button)findViewById(R.id.btSeatOk);
-
+        G1 = (ToggleButton)findViewById(R.id.tbG1);
+        G2 = (ToggleButton)findViewById(R.id.tbG2);
+        G3 = (ToggleButton)findViewById(R.id.tbG3);
+        G4 = (ToggleButton)findViewById(R.id.tbG4);
+        G5 = (ToggleButton)findViewById(R.id.tbG5);
+        G6 = (ToggleButton)findViewById(R.id.tbG6);
+        G7 = (ToggleButton)findViewById(R.id.tbG7);
 
 
         SeatOk.setOnClickListener(new View.OnClickListener() {
@@ -47,8 +68,5 @@ public class DetailReservActivity extends AppCompatActivity {
 
             }
         });
-
-
-
     }
 }
--- app/src/main/java/skhu/cse/network/omni_stadium/MainActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/MainActivity.java
@@ -3,6 +3,8 @@ package skhu.cse.network.omni_stadium;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -67,4 +69,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Auto Login Canceled", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
