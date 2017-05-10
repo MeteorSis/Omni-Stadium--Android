f02da175cf18db24a545eec4b21be65d59771728
SolbiZZang <thfql5481@naver.com>
2017-05-11 01:43:00 +0900

    Change chicken activity

--- app/src/main/java/skhu/cse/network/omni_stadium/ChickenActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/ChickenActivity.java
@@ -2,6 +2,9 @@ package skhu.cse.network.omni_stadium;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ChickenActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class ChickenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_chicken_main);
+        ImageView fchick = (ImageView)findViewById(R.id.ivfchicken);
+        ImageView ychick = (ImageView)findViewById(R.id.ivychicken);
+        ImageView gchick = (ImageView)findViewById(R.id.ivgchicken);
+        ImageView hchick = (ImageView)findViewById(R.id.ivhalfchicken);
+        ImageView hotchick = (ImageView)findViewById(R.id.ivhotchicken);
+        ImageView pachick = (ImageView)findViewById(R.id.ivpachicken);
+        ImageView currychick = (ImageView)findViewById(R.id.ivcurrychicken);
+        ImageView gokmul = (ImageView)findViewById(R.id.ivgokmul);
+        ImageView honey = (ImageView)findViewById(R.id.ivhoney);
+        ImageView cheeze = (ImageView)findViewById(R.id.ivcheeze);
+
+
+        Glide.with(this).load(R.drawable.fchicken).into(fchick);
+        Glide.with(this).load(R.drawable.ychicken).into(ychick);
+        Glide.with(this).load(R.drawable.gchicken).into(gchick);
+        Glide.with(this).load(R.drawable.halfchicken).into(hchick);
+        Glide.with(this).load(R.drawable.papachick).into(pachick);
+        Glide.with(this).load(R.drawable.size_hotchicken).into(hotchick);
+        Glide.with(this).load(R.drawable.size_currychicken).into(currychick);
+        Glide.with(this).load(R.drawable.size_gokmul).into(gokmul);
+        Glide.with(this).load(R.drawable.size_honeychicken).into(honey);
+        Glide.with(this).load(R.drawable.size_cheeze).into(cheeze);
+
+
     }
 }
