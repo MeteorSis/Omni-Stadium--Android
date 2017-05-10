e712d7b98f9694ce575bca9472cf80e4dd8495cc
SolbiZZang <thfql5481@naver.com>
2017-05-10 20:43:14 +0900

    Add  NFC  image  file

--- app/src/main/java/skhu/cse/network/omni_stadium/NFCActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/NFCActivity.java
@@ -3,6 +3,9 @@ package skhu.cse.network.omni_stadium;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class NFCActivity extends AppCompatActivity {
 
@@ -10,6 +13,7 @@ public class NFCActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nfc_main);
+        Glide.with(this).load(R.drawable.nfctag).into((ImageView)findViewById(R.id.ivNFC));
     }
 }
 
