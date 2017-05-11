462886abc2f7f4d67d7259df7d24f436f24d4edb
SolbiZZang <thfql5481@naver.com>
2017-05-11 21:05:27 +0900

    Add child menu list

--- app/src/main/java/skhu/cse/network/omni_stadium/OrderActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/OrderActivity.java
@@ -8,35 +8,85 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class OrderActivity extends AppCompatActivity{
-    @Override
+
+    private ArrayList<String> parent_menu = new ArrayList<String>();
+    private HashMap<String, ArrayList<String>> child_menu = new HashMap<String, ArrayList<String>>();
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_main);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ExpandableListView lvorder = (ExpandableListView)findViewById(R.id.explv_order);
+        parent_menu.add("치킨");
+        parent_menu.add("피자");
+        parent_menu.add("햄버거");
+        parent_menu.add("맥주");
 
-       String[] menu = {"치킨","피자", "햄버거","맥주"};
-        ListView order = (ListView)findViewById(R.id.lvorder);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
-        order.setAdapter(adapter);
+
+        ArrayList<String> chicken = new ArrayList<String>();
+        chicken.add("후라이드치킨");
+        chicken.add("양념치킨");
+        chicken.add("간장치킨");
+        chicken.add("반반치킨");
+        chicken.add("파닭");
+        chicken.add("핫치킨");
+        chicken.add("커리치킨");
+        chicken.add("곡물치킨");
+        chicken.add("허니갈릭스치킨");
+        chicken.add("치즐링치킨");
+
+        ArrayList<String> pizza = new ArrayList<String>();
+        pizza.add(" 콤비네이션피자");
+        pizza.add("페퍼로니피자");
+        pizza.add("치즈피자");
+        pizza.add("불고기피자");
+        pizza.add("포테이토피자");
+        pizza.add("고구마피자");
+        pizza.add("도이치소시지피자");
+        pizza.add("스위트골드피자");
+        pizza.add("핫스파이스피자");
+        pizza.add("바이트골드피자");
+
+        ArrayList<String> hamburger = new ArrayList<String>();
+        hamburger.add("햄버거");
+        hamburger.add("불고기 버거");
+        hamburger.add("치즈 버거");
+        hamburger.add("크런치치킨 버거");
+        hamburger.add("와일드갈릭 버거");
+        hamburger.add("갈릭스테이크 버거");
+        hamburger.add("와퍼");
+        hamburger.add("불고기 와퍼");
+        hamburger.add("치즈 와퍼");
+        hamburger.add("통새우 와퍼");
 
 
-        order.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ArrayList<String> beer = new ArrayList<String>();
+        beer.add("생맥주");
+        beer.add("크림 생맥주");
+        beer.add("더치 맥주");
+        beer.add("자몽 맥주");
+        beer.add("라임 맥주");
+        beer.add("청포도 맥주");
+        beer.add("병맥주");
 
-              /*  int mid = view.getId();
+        child_menu.put(parent_menu.get(0), chicken);
+        child_menu.put(parent_menu.get(1), pizza);
+        child_menu.put(parent_menu.get(2),  hamburger);
+        child_menu.put(parent_menu.get(3),  beer);
+        lvorder.setAdapter(new ExplvAdapter(this, parent_menu, child_menu));
 
-                switch(mid){
 
-                }*/
-            }
-        });
+
 
 
     }
