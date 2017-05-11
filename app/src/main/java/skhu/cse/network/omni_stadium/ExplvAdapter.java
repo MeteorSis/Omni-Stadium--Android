b21ee1993d52a1f728204aff0d357fc99ebed39b
song <earlydrink@gmail.com>
2017-05-11 19:32:55 +0900

    add Mypage with ExpandableListArray

---
+++ app/src/main/java/skhu/cse/network/omni_stadium/ExplvAdapter.java
@@ -0,0 +1,99 @@
+package skhu.cse.network.omni_stadium;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ExplvAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private ArrayList<String> arrayGroup;
+    private HashMap<String, ArrayList<String>> arrayChild;
+
+    public ExplvAdapter(Context context, ArrayList<String> arrayGroup, HashMap<String, ArrayList<String>> arrayChild){
+        super();
+        this.context = context;
+        this.arrayGroup = arrayGroup;
+        this.arrayChild = arrayChild;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return arrayGroup.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return arrayChild.get(arrayGroup.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return arrayGroup.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return arrayChild.get(arrayGroup.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String groupName = arrayGroup.get(groupPosition);
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.explist_group, null);
+        }
+
+        TextView expListHeader = (TextView) convertView
+                .findViewById(R.id.textGroup);
+        expListHeader.setTypeface(null, Typeface.BOLD);
+        expListHeader.setText(groupName);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        String childName = (String)getChild(groupPosition,childPosition);
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.explist_child, null);
+        }
+
+        TextView expListChild = (TextView) convertView.findViewById(R.id.textChild);
+        expListChild.setTypeface(null, Typeface.BOLD);
+        expListChild.setText(childName);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
--- app/src/main/java/skhu/cse/network/omni_stadium/MyPageActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/MyPageActivity.java
@@ -2,35 +2,44 @@ package skhu.cse.network.omni_stadium;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MyPageActivity extends AppCompatActivity {
+
+    private ArrayList<String> arrayGroup = new ArrayList<String>();
+    private HashMap<String, ArrayList<String>> arrayChild = new HashMap<String, ArrayList<String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+        ExpandableListView lvMyPage = (ExpandableListView)findViewById(R.id.explv_Setting);
+        arrayGroup.add("로그아웃");
+        arrayGroup.add("티켓관리");
+        arrayGroup.add("회원정보 관리");
 
-        String[] values = {"회원정보 수정", "자유석 해제", "티켓환불", "로그아웃", "회원탈퇴"};
+        ArrayList<String> arrayLogout = new ArrayList<String>();
+        arrayLogout.add("· 로그아웃");
 
-        ListView lvMyPage = (ListView)findViewById(R.id.lvMyPage);
+        ArrayList<String> arrayTicket = new ArrayList<String>();
+        arrayTicket.add("· 자유석 해제");
+        arrayTicket.add("· 티켓환불");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
-        lvMyPage.setAdapter(adapter);
+        ArrayList<String> arrayManage = new ArrayList<String>();
+        arrayManage.add("· 회원정보 수정");
+        arrayManage.add("· 회원탈퇴");
 
-        lvMyPage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int vid = view.getId();
+        arrayChild.put(arrayGroup.get(0), arrayLogout);
+        arrayChild.put(arrayGroup.get(1), arrayTicket);
+        arrayChild.put(arrayGroup.get(2), arrayManage);
 
-                switch(vid){
+        lvMyPage.setAdapter(new ExplvAdapter(this, arrayGroup, arrayChild));
 
-                }
-            }
-        });
+
+
     }
 
 }
