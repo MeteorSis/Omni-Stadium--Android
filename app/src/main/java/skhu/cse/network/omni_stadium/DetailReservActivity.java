fbda451bcdd7651c8d8c6dba375613c586f3c2d4
MeteorSis <uncleone@naver.com>
2017-05-14 20:55:43 +0900

    edit detailReserv

--- app/src/main/java/skhu/cse/network/omni_stadium/DetailReservActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/DetailReservActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,11 @@ public class DetailReservActivity extends AppCompatActivity {
     private boolean isCheckedInArr=false;
     private ToggleButton tempTB;
     private EditText seatInfo;
+    private char charRow;
+    private int row;
+    private CharSequence chSq_seat_no;
+    private int seat_no;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,26 +58,34 @@ public class DetailReservActivity extends AppCompatActivity {
         SeatOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(DetailReservActivity.this);
-                dlg.setTitle("예매 정보");
-                dlg.setMessage("해당 좌석을 결제 하시겠습니까?");
+                if(isCheckedInArr)
+                {
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(DetailReservActivity.this);
+                    dlg.setTitle("예매 정보");
+                    dlg.setMessage("해당 좌석을 결제 하시겠습니까?");
 
-                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                        Toast.makeText(getApplicationContext(),"결제가 완료 되었습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        Toast.makeText(getApplicationContext(),"결제가 취소 되었습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                dlg.show();
-                dlg.setCancelable(false); // 백버튼 비활성화
+                    dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            row = Character.getNumericValue(charRow);
+                            seat_no = Integer.parseInt(chSq_seat_no.toString());
+                            Toast.makeText(getApplicationContext(), "결제가 완료 되었습니다.\n" + row + "열 " + seat_no + "석", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+                    dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getApplicationContext(), "결제가 취소 되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    dlg.show();
+                    dlg.setCancelable(false); // 백버튼 비활성화
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "선택한 좌석이 없습니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -114,11 +128,11 @@ public class DetailReservActivity extends AppCompatActivity {
                 tempTB=tB;
             }
             tB.setTextColor(Color.parseColor("#FFFFFF"));
-            CharSequence tBText=tB.getText();
-            char row=tBText.charAt(0);
-            if(tBText.charAt(1)!='0')
-                row++;
-            seatInfo.setText(value+" : "+row+"행 "+tBText+"석");
+            chSq_seat_no=tB.getText();
+            charRow=chSq_seat_no.charAt(0);
+            if(chSq_seat_no.charAt(1)!='0')
+                charRow++;
+            seatInfo.setText(value+" : "+charRow+"행 "+chSq_seat_no+"석");
         }
         else
         {
