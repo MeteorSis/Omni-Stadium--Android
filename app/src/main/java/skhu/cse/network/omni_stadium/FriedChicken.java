667a1d95d3943113ba03f43486f9eb0551f3a004
SolbiZZang <thfql5481@naver.com>
2017-05-18 17:10:17 +0900

    order_fried_chicken file is  Changed

--- app/src/main/java/skhu/cse/network/omni_stadium/FriedChicken.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/FriedChicken.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FriedChicken extends AppCompatActivity {
-
+    private  int fcount; //치킨 수량을 저장하는 변수
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_fried_chicken);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+
     }
 }
