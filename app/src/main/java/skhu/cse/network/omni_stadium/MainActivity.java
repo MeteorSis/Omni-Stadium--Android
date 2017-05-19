8b4d74928c6872f1ffa8283117c91559b5e461ef
MeteorSis <uncleone@naver.com>
2017-05-19 17:26:54 +0900

    edit Login, SignUp, searchID, searchPW activity

--- app/src/main/java/skhu/cse/network/omni_stadium/MainActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -20,13 +21,19 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-
     private BackPressCloseHandler backPressCloseHandler;
+    private EditText etID, etPW;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +43,15 @@ public class MainActivity extends AppCompatActivity {
         TextView SearchId = (TextView) findViewById(R.id.tvSearchId);
         TextView SearchPw = (TextView) findViewById(R.id.tvSearchPW);
 
-        final EditText etID = (EditText)findViewById(R.id.input_id);
-        final EditText etPW = (EditText)findViewById(R.id.input_password);
+        etID = (EditText)findViewById(R.id.input_id);
+        etPW = (EditText)findViewById(R.id.input_password);
+        etPW.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                loginFunc();
+                return true;
+            }
+        });
 
         Button Login = (Button)findViewById(R.id.btlogin);
         CheckBox cbAutoLogin = (CheckBox)findViewById(R.id.check_autologin);
@@ -57,8 +71,8 @@ public class MainActivity extends AppCompatActivity {
                     String ID = etID.getText().toString();
                     String PW = etPW.getText().toString();
 
-                    editor.putString("ID",ID);
-                    editor.putString("PW",PW);
+                    editor.putString("ID", ID);
+                    editor.putString("PW", PW);
                     editor.putBoolean("Auto_Login_enabled",true);
                     editor.commit();
                 }
@@ -76,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             etID.setText(autoSetting.getString("ID",""));
             etPW.setText(autoSetting.getString("PW",""));
             cbAutoLogin.setChecked(true);
+            loginFunc();
         }
 
 
@@ -106,50 +121,94 @@ public class MainActivity extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                startActivity(intent);
+                loginFunc();
             }
         });
 
         backPressCloseHandler = new BackPressCloseHandler(this);
     }
+    private void loginFunc()
+    {
+        String strID=etID.getText().toString();
+        String strPW=etPW.getText().toString();
+        String IDregex = "^[a-zA-Z]{1}[a-zA-Z0-9_]{4,11}$";
+        Pattern IDpattern = Pattern.compile(IDregex);
+        Matcher IDmatcher = IDpattern.matcher(strID);
 
+        if(strID.matches("") || strPW.matches("") || !IDmatcher.find() || strPW.length()<6)
+            Toast.makeText(MainActivity.this, "아이디 혹은 비밀번호가 잘못 입력되었습니다.", Toast.LENGTH_SHORT).show();
+        else
+            new LoginTask().execute(etID.getText().toString(), etPW.getText().toString());
+    }
 
-    private class Login extends AsyncTask<String, Void, JSONObject>{
+    private class LoginTask extends AsyncTask<String, Void, JSONObject>
+    {
         @Override
         protected JSONObject doInBackground(String... params) {
             URL url = null;
-            HttpURLConnection urlConnection = null;
+            HttpURLConnection httpCon = null;
             JSONObject getJSON = null;
 
             try {
-                url = new URL("http://172.20.10.2:8080/app/login.jsp");
-                urlConnection = (HttpURLConnection) url.openConnection();
+                url = new URL("http://192.168.123.167:51223/AndroidClientLogInRequestPost");
+                httpCon = (HttpURLConnection) url.openConnection();
 
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setDoInput(true);
-                urlConnection.setDoOutput(true);
-                urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                httpCon.setRequestMethod("POST");
+                httpCon.setDoInput(true);
+                httpCon.setDoOutput(true);
+                httpCon.setConnectTimeout(2000);
+                httpCon.setReadTimeout(2000);
+
+                httpCon.setRequestProperty("Cache-Control", "no-cache");
+                //서버에 요청할 Response Data Type
+                httpCon.setRequestProperty("Accept", "application/json");
+                //서버에 전송할 Data Type
+                //httpCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                httpCon.setRequestProperty("Content-Type", "application/json");
 
                 JSONObject outJson = new JSONObject();
-                outJson.put("ID", params[0]);
-                outJson.put("PW", params[1]);
+                outJson.put("아이디", params[0]);
+                outJson.put("비밀번호", params[1]);
 
-                OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-                out.write(outJson.toString().getBytes());
+                OutputStream out = new BufferedOutputStream(httpCon.getOutputStream());
+                out.write(outJson.toString().getBytes("UTF-8"));
                 out.flush();
+
+                int responseCode = httpCon.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream=httpCon.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    StringBuilder result = new StringBuilder();
+                    while((line = bufferedReader.readLine()) != null)
+                        result.append(line);
+                    inputStream.close();
+                    getJSON = new JSONObject(result.toString());
+                }
+            } catch (Exception e) {
+            } finally {
+                httpCon.disconnect();
             }
-             catch (Exception e) {
-                }
-            finally {
-                    urlConnection.disconnect();
-                }
-                return getJSON;
+            return getJSON;
         }
 
         @Override
         protected void onPostExecute(JSONObject jsonObject) {
             super.onPostExecute(jsonObject);
+            try {
+                int result=jsonObject.getInt("결과");
+                if(result==0)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                    startActivity(intent);
+                }
+                else if(result==1)
+                    Toast.makeText(MainActivity.this, "접속 중인 아이디가 있습니다.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "아이디 혹은 비밀번호가 잘못 입력되었습니다.", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+
+            }
         }
     }
 
--- app/src/main/java/skhu/cse/network/omni_stadium/SidActivity.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/SidActivity.java
@@ -7,10 +7,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONObject;
 
@@ -22,6 +23,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SidActivity extends AppCompatActivity {
+    private EditText etName, etPhoneFront, etPhoneMiddle, etPhoneBack,
+            etBirthYYYY, etBirthMM, etBirthDD;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,66 +33,99 @@ public class SidActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Button btOK = (Button)findViewById(R.id.btSidOK);
 
+        etName = (EditText)findViewById(R.id.etName);
+        etPhoneFront = (EditText)findViewById(R.id.etPhoneFront);
+        etPhoneMiddle = (EditText)findViewById(R.id.etPhoneMiddle);
+        etPhoneBack = (EditText)findViewById(R.id.etPhoneBack);
+        etBirthYYYY = (EditText)findViewById(R.id.etBirthYYYY);
+        etBirthMM = (EditText)findViewById(R.id.etBirthMM);
+        etBirthDD = (EditText)findViewById(R.id.etBirthDD);
+
+        etBirthDD.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                patternCheck();
+                return true;
+            }
+        });
+
         btOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText etName = (EditText)findViewById(R.id.etName);
-                EditText etPhone = (EditText)findViewById(R.id.etPhone);
-                EditText etBdate = (EditText)findViewById(R.id.etBdate);
-
-                String NameData = etName.getText().toString();
-                String PhoneData = etPhone.getText().toString();
-                String BdateData = etBdate.getText().toString();
-
-                String[] SidData = {NameData, PhoneData, BdateData};
-                String Empty = "";
-
-                if(SidData[0].matches("")&& Empty.matches("")){
-                    Empty = Empty + "이름";
-                }
-                else{
-                    String Nameregex = "^[가-힣]{2,4}$";
-                    Pattern Namepattern = Pattern.compile(Nameregex);
-                    Matcher Namematcher = Namepattern.matcher(NameData);
-                    if (!Namematcher.find()&& Empty.matches("")){
-                        Empty = Empty + "이름";
-                    }
-                }
-                if(SidData[1].matches("")&& Empty.matches("")){
-                    Empty = Empty + "핸드폰 번호";
-                }
-                else{
-                    String Phoneregex = "010([0-9]{3,4})([0-9]{4})";
-                    Pattern Phonepattern = Pattern.compile(Phoneregex);
-                    Matcher Phonematcher = Phonepattern.matcher(PhoneData);
-                    if (!Phonematcher.find()&& Empty.matches("")){
-                        Empty = Empty + "핸드폰 번호";
-                    }
-                }
-                if(SidData[2].matches("")&& Empty.matches("")){
-                    Empty = Empty + "생년월일";
-                }
-                else{
-                    String Bdateregex = "(19|20)\\d{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$";
-                    Pattern Bdatepattern = Pattern.compile(Bdateregex);
-                    Matcher Bdatematcher = Bdatepattern.matcher(BdateData);
-                    if (!Bdatematcher.find()&& Empty.matches("")){
-                        Empty = Empty + "생년월일";
-                    }
-                }
-
-                if(Empty.matches("")){
-                    //Toast.makeText(getApplicationContext(), "입력 완료", Toast.LENGTH_SHORT).show();
-                    new SidActivity.FindID().execute(SidData[0], SidData[1], SidData[2]);
-                }
-                else{
-                    new AlertDialog.Builder(SidActivity.this)
-                            .setMessage(Empty+"을(를) 다시 확인해주세요.")
-                            .setPositiveButton("OK",null)
-                            .show();
-                }
+                patternCheck();
             }
         });
+    }
+
+    private void patternCheck() {
+        String NameData = etName.getText().toString();
+        String PhoneFrontData = etPhoneFront.getText().toString();
+        String PhoneMiddleData = etPhoneMiddle.getText().toString();
+        String PhoneBackData = etPhoneBack.getText().toString();
+        String BirthYYYYData = etBirthYYYY.getText().toString();
+        String BirthMMData = etBirthMM.getText().toString();
+        String BirthDDData = etBirthDD.getText().toString();
+        String Empty = "";
+
+        if(NameData.matches("")){
+            Empty = Empty + "이름 ";
+        }
+        else{
+            String Nameregex = "^[가-힣]{2,4}$";
+            Pattern Namepattern = Pattern.compile(Nameregex);
+            Matcher Namematcher = Namepattern.matcher(NameData);
+            if (!Namematcher.find()){
+                Empty = Empty + "이름 ";
+            }
+        }
+
+        if(PhoneFrontData.matches("") || PhoneMiddleData.matches("") || PhoneBackData.matches("")){
+            Empty = Empty + "핸드폰 번호 ";
+        }
+        else{
+            String PhoneFrontregex = "^01[016789]$";
+            String PhoneMiddleregex="^[0-9]{3,4}$";
+            String PhoneBackregex="^[0-9]{3,4}$";
+            Pattern PhoneFrontpattern = Pattern.compile(PhoneFrontregex);
+            Pattern PhoneMiddlepattern = Pattern.compile(PhoneMiddleregex);
+            Pattern PhoneBackpattern = Pattern.compile(PhoneBackregex);
+            Matcher PhoneFrontmatcher = PhoneFrontpattern.matcher(PhoneFrontData);
+            Matcher PhoneMiddlematcher = PhoneMiddlepattern.matcher(PhoneMiddleData);
+            Matcher PhoneBackmatcher = PhoneBackpattern.matcher(PhoneBackData);
+            if (!PhoneFrontmatcher.find() || !PhoneMiddlematcher.find() || !PhoneBackmatcher.find()){
+                Empty = Empty + "핸드폰 번호 ";
+            }
+        }
+
+        if(BirthYYYYData.matches("") || BirthMMData.matches("") || BirthDDData.matches("")){
+            Empty = Empty + "생년월일 ";
+        }
+        else{
+            String BYYYYregex="^(19|20)[0-9]{2}$";
+            String BMMregex="^(0[1-9]|1[012]|[1-9])$";
+            String BDDregex="^([1-9]|0[1-9]|[12][0-9]|3[01])$";
+
+            Pattern BYYYYpattern = Pattern.compile(BYYYYregex);
+            Pattern BMMpattern = Pattern.compile(BMMregex);
+            Pattern BDDpattern = Pattern.compile(BDDregex);
+            Matcher BYYYYmatcher = BYYYYpattern.matcher(BirthYYYYData);
+            Matcher BMMmatcher = BMMpattern.matcher(BirthMMData);
+            Matcher BDDmatcher = BDDpattern.matcher(BirthDDData);
+            if (!BYYYYmatcher.find() || !BMMmatcher.find() || !BDDmatcher.find()){
+                Empty = Empty + "생년월일 ";
+            }
+        }
+
+        if(Empty.matches("")){
+            //Toast.makeText(getApplicationContext(), "입력 완료", Toast.LENGTH_SHORT).show();
+            new SidActivity.FindID().execute(NameData, PhoneFrontData+"-"+PhoneMiddleData+"-"+PhoneBackData, BirthYYYYData+"-"+BirthMMData+"-"+BirthDDData);
+        }
+        else{
+            new AlertDialog.Builder(SidActivity.this)
+                    .setMessage(Empty+"을(를) 다시 확인해주세요.")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
     }
 
     private class FindID extends AsyncTask<String, Void, JSONObject> {
