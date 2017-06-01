91ab6471d66125e1772e110ab616788f3811c0f8
MeteorSis <uncleone@naver.com>
2017-06-01 22:36:35 +0900

    edit OmniApplication Class

--- app/src/main/java/skhu/cse/network/omni_stadium/OmniApplication.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/OmniApplication.java
@@ -13,7 +13,6 @@ public class OmniApplication extends Application {
     private Integer seat_row=null;
     private Integer seat_no=null;
 
-
     public void setMem_id(String mem_id)
     {
         this.mem_id=mem_id;
@@ -35,28 +34,35 @@ public class OmniApplication extends Application {
     public Integer getTicket_no() {
         return ticket_no;
     }
-    public void setTicket_no(Integer ticket_no) {
+    public void setTicket_no(Integer ticket_no)
+    {
         this.ticket_no = ticket_no;
     }
 
-    public String getSeat_zone() {
+    public String getSeat_zone()
+    {
         return seat_zone;
     }
-    public void setSeat_zone(String seat_zone) {
+    public void setSeat_zone(String seat_zone)
+    {
         this.seat_zone = seat_zone;
     }
 
-    public Integer getSeat_row() {
+    public Integer getSeat_row()
+    {
         return seat_row;
     }
-    public void setSeat_row(Integer seat_row) {
+    public void setSeat_row(Integer seat_row)
+    {
         this.seat_row = seat_row;
     }
 
-    public Integer getSeat_no() {
+    public Integer getSeat_no()
+    {
         return seat_no;
     }
-    public void setSeat_no(Integer seat_no) {
+    public void setSeat_no(Integer seat_no)
+    {
         this.seat_no = seat_no;
     }
 }
