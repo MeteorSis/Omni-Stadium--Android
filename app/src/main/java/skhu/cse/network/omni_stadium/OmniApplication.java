5555a7ece581fb729445564f5fd89405b26008b5
MeteorSis <uncleone@naver.com>
2017-06-01 22:02:28 +0900

    edit OmniApplication class

--- app/src/main/java/skhu/cse/network/omni_stadium/OmniApplication.java
+++ app/src/main/java/skhu/cse/network/omni_stadium/OmniApplication.java
@@ -5,8 +5,7 @@ import android.app.Application;
 public class OmniApplication extends Application {
 
     private String mem_id=null;
-    private String name=null;
-    private String
+    private String mem_name=null;
 
 
     public void setMem_id(String mem_id)
@@ -16,6 +15,15 @@ public class OmniApplication extends Application {
     public String getMem_id()
     {
         return mem_id;
+    }
+
+    public void setMem_name(String mem_name)
+    {
+        this.mem_name=mem_name;
+    }
+    public String getMem_name()
+    {
+        return mem_name;
     }
 
 
